389eeb82ba3f47f816c11de204d8273038ef8a07
Sofiia Maksymchuk <sofiiamaksymchuk@Sofiias-MacBook-Air.local>
2026-04-26 22:03:04 -0400

    add selected movie modal and toast

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,27 +1,45 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { keepPreviousData, useQuery } from "@tanstack/react-query";
 import ReactPaginate from "react-paginate";
+import toast, { Toaster } from "react-hot-toast";
 import css from "./App.module.css";
+import type { Movie } from "../../types/movie";
 import { fetchMovies } from "../../services/movieService";
 import SearchBar from "../SearchBar/SearchBar";
 import MovieGrid from "../MovieGrid/MovieGrid";
+import MovieModal from "../MovieModal/MovieModal";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
 export default function App() {
   const [query, setQuery] = useState<string>("");
   const [page, setPage] = useState<number>(1);
+  const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
 
   const { data, isLoading, isError, isFetching } = useQuery({
     queryKey: ["movies", query, page],
     queryFn: () => fetchMovies(query, page),
-    enabled: query !== "",
+    enabled: query.trim() !== "",
     placeholderData: keepPreviousData,
   });
+
+  useEffect(() => {
+    if (data && data.results.length === 0) {
+      toast.error("No movies found.");
+    }
+  }, [data]);
 
   const handleSearch = (newQuery: string) => {
     setQuery(newQuery);
     setPage(1);
+  };
+
+  const handleSelectMovie = (movie: Movie) => {
+    setSelectedMovie(movie);
+  };
+
+  const handleCloseModal = () => {
+    setSelectedMovie(null);
   };
 
   const totalPages = data?.total_pages ? Math.min(data.total_pages, 500) : 0;
@@ -36,7 +54,7 @@ export default function App() {
 
       {data && data.results.length > 0 && (
         <>
-          <MovieGrid movies={data.results} onSelect={() => {}} />
+          <MovieGrid movies={data.results} onSelect={handleSelectMovie} />
 
           {totalPages > 1 && (
             <ReactPaginate
@@ -49,15 +67,16 @@ export default function App() {
               activeClassName={css.active}
               nextLabel="→"
               previousLabel="←"
-              breakLabel="..."
             />
           )}
         </>
       )}
 
-      {data && data.results.length === 0 && (
-        <p className={css.text}>No movies found.</p>
+      {selectedMovie && (
+        <MovieModal movie={selectedMovie} onClose={handleCloseModal} />
       )}
+
+      <Toaster position="top-right" />
 
       {isFetching && !isLoading && <Loader />}
     </div>
